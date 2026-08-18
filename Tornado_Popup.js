365f2d8d66c6933651deb32904c9aea7609e9f30
Apple Bravetree <applebravetree@gmail.com>
2026-08-18 19:57:34 +0200

    Update Tornado_Popup.js

--- Tornado_Popup.js
+++ Tornado_Popup.js
@@ -8,7 +8,8 @@ function tornadoClicked(e) {
     var txtWidth = ""
     var txtLength = ""
     var txtDuration = ""
-    var txtDeathInjuries = ""
+    var txtDeaths = ""
+    var txtInjuries = ""
     if (prop.hour != -99) {
         txtTime = numLeng(prop.hour, 2) + ":" + numLeng(prop.minute, 2) + " " + prop.timezone + " "
     }
@@ -41,8 +42,14 @@ function tornadoClicked(e) {
     if (prop.duration != -99) {
         txtDuration = "<tr><th>Duration</th><td>" + prop.duration + " min</td></tr> "
     }
+    if (prop.deaths != -99) {
+        txtDeaths = "<tr><th>Deaths</th><td>" + prop.deaths + "</td></tr> "
+    }
+    if (prop.injuries != -99) {
+        txtInjuries = "<tr><th>Injuries</th><td>" + prop.injuries + "</td></tr> "
+    }
     L.popup()
         .setLatLng(e.latlng)
-        .setContent("<table><thead><tr><th class='popup_header_F" + prop.rating + "' colspan='99'><h2>EF" + prop.rating + " Tornado - " + prop.year + "/" + numLeng(prop.month, 2) + "/" + numLeng(prop.day, 2) + "</h2></th></tr></thead> <tbody><tr><th>Start time</th><td>" + txtTime + txtDate + "</td></tr> <tr><th>Rating</th><td>EF" + prop.rating + "</td></tr> " + txtWindspeed + txtWidth + txtLength + txtDuration + "<tr><th>Deaths/Injuries</th><td>" + prop.deaths + "/" + prop.injuries + "</td></tr> <tr><th>Comments</th><td>" + prop.comments + "</td></tr></tbody><table>")
+        .setContent("<table><thead><tr><th class='popup_header_F" + prop.rating + "' colspan='99'><h2>EF" + prop.rating + " Tornado - " + prop.year + "/" + numLeng(prop.month, 2) + "/" + numLeng(prop.day, 2) + "</h2></th></tr></thead> <tbody><tr><th>Start time</th><td>" + txtTime + txtDate + "</td></tr> <tr><th>Rating</th><td>EF" + prop.rating + "</td></tr> " + txtWindspeed + txtWidth + txtLength + txtDuration + txtDeaths + txtInjuries + "<tr><th>Comments</th><td>" + prop.comments + "</td></tr></tbody><table>")
         .openOn(map);
 }
